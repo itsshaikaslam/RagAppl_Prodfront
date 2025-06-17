965af12946915777bcf867058cc506b7ca1b8bc0
itsshaikaslam <itsshaikaslam@gmail.com>
2025-06-17 13:17:14 +0530

    initial commit4

--- start.py
+++ start.py
@@ -7,8 +7,8 @@ port = os.environ.get('PORT', '8150')
 print(f"🔍 Starting frontend on port: {port}")
 
 subprocess.run([
-    sys.executable, '-m', 'uvicorn', 
-    'app.main:app', 
+    sys.executable, '-m', 'streamlit', 'run',
+    'frontend\app.py', 
     '--host', '0.0.0.0', 
     '--port', port
 ])
